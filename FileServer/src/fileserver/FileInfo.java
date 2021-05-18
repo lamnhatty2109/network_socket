41634acc401b1e3f42b93af2e491db0f921233aa
lamnhatty2109 <ty@emobi.biz>
2021-05-18 22:21:54 +0700

    test

---
+++ FileServer/src/fileserver/FileInfo.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fileserver;
+
+/**
+ *
+ * @author lamnh
+ */
+public class FileInfo {
+    private static final long serialVersionUID = 1L;
+
+    public String getDestinationDirectory() {
+        return destinationDirectory;
+    }
+
+    public void setDestinationDirectory(String destinationDirectory) {
+        this.destinationDirectory = destinationDirectory;
+    }
+
+    public String getSourceDirectory() {
+        return sourceDirectory;
+    }
+
+    public void setSourceDirectory(String sourceDirectory) {
+        this.sourceDirectory = sourceDirectory;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public int getPiecesOfFile() {
+        return piecesOfFile;
+    }
+
+    public void setPiecesOfFile(int piecesOfFile) {
+        this.piecesOfFile = piecesOfFile;
+    }
+
+    public int getLastByteLength() {
+        return lastByteLength;
+    }
+
+    public void setLastByteLength(int lastByteLength) {
+        this.lastByteLength = lastByteLength;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public FileInfo() {
+    }
+
+    private String destinationDirectory;
+    private String sourceDirectory;
+    private String filename;
+    private long fileSize;
+    private int piecesOfFile;
+    private int lastByteLength;
+    private String status;
+}
