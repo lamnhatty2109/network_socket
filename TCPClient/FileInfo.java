d61a9a65ed752dd22187e1f2357de8a0e95a1f26
Đặng Xuân Danh <57908643+DangXuanDanh@users.noreply.github.com>
2021-05-20 14:19:40 +0700

    TCPClient

---
+++ TCPClient/FileInfo.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author lamnh
+ */
+public class FileInfo implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    private String destinationDirectory;
+    private String sourceDirectory;
+    private String filename;
+    private long fileSize;
+    private int piecesOfFile;
+    private int lastByteLength;
+    private String status;
+
+    private String host;
+    private int port;
+    
+    public String getDestinationDirectory() {
+        return destinationDirectory;
+    }
+
+    public void setDestinationDirectory(String destinationDirectory) {
+        this.destinationDirectory = destinationDirectory;
+    }
+
+    public String getSourceDirectory() {
+        return sourceDirectory;
+    }
+
+    public void setSourceDirectory(String sourceDirectory) {
+        this.sourceDirectory = sourceDirectory;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public int getPiecesOfFile() {
+        return piecesOfFile;
+    }
+
+    public void setPiecesOfFile(int piecesOfFile) {
+        this.piecesOfFile = piecesOfFile;
+    }
+
+    public int getLastByteLength() {
+        return lastByteLength;
+    }
+
+    public void setLastByteLength(int lastByteLength) {
+        this.lastByteLength = lastByteLength;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setInfo(String h, int p){
+        host = h;
+        port = p;
+    }
+
+    public String getHost(){
+        return host;
+    }
+    public int getPort(){
+        return port;
+    }
+
+    public FileInfo() {
+    }
+
+}
+
